d345efa3b2441ee87b5cc73bb591c8deb89e8c4d
hucilluc <katehenderson@gmx.co.uk>
2021-01-21 18:47:38 +0000

    Roof and icicles inactivated

---
+++ Cave World/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/Kate/Desktop/Github/Cave-World/Cave World/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1653812091431158487.json
